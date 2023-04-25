deed4d5558407155d48ddaa2e3d3a7df2dde3560
Luiz André Vieira da Rosa <luizandrevr@gmail.com>
2023-04-25 15:01:56 -0300

    feat: created a types file

---
+++ utils/types.ts
@@ -0,0 +1,10 @@
+export type Player = {
+  id: number;
+  chips: number;
+  game_id: number;
+  name: string;
+  person_id: number;
+  quantity_rebuy: number;
+  re_buy_value: number;
+  buy_in_value: number;
+};
