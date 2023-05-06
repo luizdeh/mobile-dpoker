d5ff080cadb5b6a1f3edea3d7ecb74e4ebb75b63
Luiz André Vieira da Rosa <luizandrevr@gmail.com>
2023-05-06 09:46:26 -0300

    feat(players): deleted useless component

--- screens/Players.tsx
+++ screens/Players.tsx
@@ -12,13 +12,15 @@ import {
 } from "native-base";
 import { getPlayers } from "../utils/fetchPlayers";
 import { addPlayer } from "../utils/addPlayer";
-import { MaterialIcons } from "@expo/vector-icons";
+import { MaterialIcons, Entypo } from "@expo/vector-icons";
 import { PlayerList } from "../lib/types";
 import RegisteredPlayer from "../components/RegisteredPlayer";
+import { ScrollView } from "react-native";
 
 export default function PlayersList() {
   const [playerList, setPlayerList] = useState<PlayerList[]>([]);
   const [refState, setRefState] = useState(false);
+  const [showAddPlayerButton, setShowAddPlayerButton] = useState(false);
   const ref = useRef<HTMLInputElement | null>(null);
 
   const onClear = () => {
@@ -51,76 +53,91 @@ export default function PlayersList() {
   return (
     <Box
       _dark={{ bg: "blueGray.900" }}
-      _light={{ bg: "blueGray.50" }}
+      _light={{ bg: "white" }}
       px={4}
       py={2}
       flex={1}
     >
-      <Center>
-        <HStack
-          my={2}
-          space={6}
-          justifyItems="space-between"
-          alignItems="center"
-        >
-          <Input
-            style={{
-              height: 40,
-              paddingLeft: 6,
-              borderWidth: 1,
-              backgroundColor: "white",
-            }}
-            ref={ref}
-            autoFocus={true}
-            keyboardType="default"
-            placeholder=" ... Daniel Negreanu"
-            onChange={handleNewInput}
-          />
-          <IconButton
-            _icon={{
-              as: MaterialIcons,
-              name: "cancel",
-              color: "rose.400",
-              size: "lg",
-            }}
-            onPress={() => onClear()}
-            isDisabled={!refState}
-          />
-        </HStack>
-        <Button
-          onPress={() => addPerson()}
-          isDisabled={!refState}
-          width="80%"
-          colorScheme="blueGray"
-          my="2"
-        >
-          ADD NEW PLAYER
-        </Button>
-      </Center>
-      <Divider
-        my="2"
-        _light={{
-          bg: "muted.600",
-        }}
-        _dark={{
-          bg: "muted.50",
-        }}
-      />
-      <br />
-      <br />
-      <VStack space={2} alignItems="center" width="100%">
-        {playerList ? (
-          playerList.map((item: PlayerList) => (
-            <RegisteredPlayer
-              key={item.id}
-              player={item}
-              updatePlayers={setPlayerList}
+      {showAddPlayerButton ? (
+        <Center>
+          <HStack
+            my={2}
+            space={6}
+            justifyItems="space-between"
+            alignItems="center"
+          >
+            <Input
+              style={{
+                height: 40,
+                paddingLeft: 6,
+                borderWidth: 1,
+                backgroundColor: "white",
+              }}
+              ref={ref}
+              autoFocus={true}
+              keyboardType="default"
+              placeholder=" ... Daniel Negreanu"
+              onChange={handleNewInput}
             />
-          ))
-        ) : (
-          <Text>No players registered yet.</Text>
-        )}
-      </VStack>
+            <IconButton
+              _icon={{
+                as: Entypo,
+                name: "erase",
+                color: "blueGray.400",
+                size: "lg",
+              }}
+              onPress={() => onClear()}
+              isDisabled={!refState}
+            />
+            <IconButton
+              _icon={{
+                as: MaterialIcons,
+                name: "clear",
+                color: "rose.400",
+                size: "xl",
+              }}
+              onPress={() => setShowAddPlayerButton(false)}
+            />
+          </HStack>
+          <Button
+            onPress={() => addPerson()}
+            isDisabled={!refState}
+            width="80%"
+            colorScheme="blueGray"
+            my="2"
+          >
+            ADD NEW PLAYER
+          </Button>
+        </Center>
+      ) : (
+        <Center>
+          <Button
+            onPress={() => setShowAddPlayerButton(true)}
+            width="80%"
+            colorScheme="blueGray"
+            variant="subtle"
+            my="2"
+          >
+            ADD NEW PLAYER
+          </Button>
+        </Center>
+      )}
+      <br />
+      <ScrollView>
+        <VStack space={2} alignItems="center" width="95%">
+          {playerList ? (
+            playerList.map((item: PlayerList) => (
+              <RegisteredPlayer
+                key={item.id}
+                player={item}
+                updatePlayers={setPlayerList}
+              />
+            ))
+          ) : (
+            <Text>No players registered yet.</Text>
+          )}
+        </VStack>
+      </ScrollView>
     </Box>
   );
 }
