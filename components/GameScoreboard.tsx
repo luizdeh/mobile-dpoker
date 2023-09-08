aef8d626097a333e55ebee71f069c35a679cfc34
Luiz André Vieira da Rosa <luizandrevr@gmail.com>
2023-09-08 01:28:22 -0300

    Add 'payment plan' to GameScoreboard
    This consists of the UI component that renders the payments, as well
    as pulling the logic needed from payments.ts

--- components/GameScoreboard.tsx
+++ components/GameScoreboard.tsx
@@ -10,6 +10,24 @@ interface Prop {
 
 export default function GameScoreboard({ game, index }: Prop) {
   const [showStats, setShowStats] = useState(false);
+  const [showPayments, setShowPayments] = useState(false);
+  const [payments, setPayments] = useState<any[]>([]);
+
+  useEffect(() => {
+    if (showStats) {
+      console.log(`game #${index + 1}`)
+      console.log({ game })
+      const all = doItAll(game)
+      setPayments(all)
+      console.log({ all })
+    }
+  }, [showStats])
+
+  const abbreviateName = (player: string) => {
+    const [name, ...lastName] = player.split(' ').filter(Boolean);
+    return lastName.length && player.length >= 11 ? `${name} ${lastName[0][0]}.` : `${name} ${lastName}`;
+  }
+
   return (
     <VStack my="2">
       <HStack py="2" alignItems="center" backgroundColor="blueGray.600">
@@ -48,19 +66,8 @@ export default function GameScoreboard({ game, index }: Prop) {
       </HStack>
       {showStats ? (
         <VStack>
-          <HStack
-            space={2}
-            backgroundColor="teal.600"
-            p={1}
-            alignItems="center"
-          >
-            <Text
-              flex={2}
-              textAlign="left"
-              color="white"
-              fontWeight="semibold"
-              fontSize="xs"
-            >
+          <HStack space={2} backgroundColor="teal.600" p={1} alignItems="center">
+            <Text flex={2} textAlign="left" color="white" fontWeight="semibold" fontSize="xs">
               PLAYER
             </Text>
             <Text flex={1} textAlign="center">
@@ -70,50 +77,33 @@ export default function GameScoreboard({ game, index }: Prop) {
               <FontAwesome name="database" size={14} color="white" />
             </Text>
             <Text flex={1} textAlign="center">
-              <FontAwesome5
-                flex={1}
-                name="percentage"
-                size={14}
-                color="white"
-              />
+              <FontAwesome5 flex={1} name="percentage" size={14} color="white" />
             </Text>
             <Text flex={1} textAlign="center">
-              <FontAwesome5
-                flex={1}
-                name="arrow-down"
-                size={14}
-                color="white"
-              />
+              <FontAwesome5 flex={1} name="arrow-down" size={14} color="white" />
             </Text>
             <Text flex={1} textAlign="center">
               <FontAwesome5 flex={1} name="arrow-up" size={14} color="white" />
             </Text>
             <Text flex={1} textAlign="center">
-              <Entypo flex={1} name="credit" size={16} color="white" />
+              <Entypo flex={1} name="credit" size={16} color={showPayments ? 'white' : 'blueGray.200'} onPress={() => setShowPayments((state) => !state)} />
             </Text>
           </HStack>
           {game.active_players
             .sort((a: any, b: any) => b.profit - a.profit)
             .map((player: any, idx: number) => {
-              const [name, ...lastName] = player.name
-                .split(" ")
-                .filter(Boolean);
-              const myName =
-                lastName.length && player.name.length >= 11
-                  ? `${name} ${lastName[0][0]}.`
-                  : `${name} ${lastName}`;
               return (
                 <HStack
                   key={idx}
                   space={2}
                   p={1}
-                  backgroundColor={idx % 2 === 0 ? "white" : "teal.50"}
+                  backgroundColor={idx % 2 === 0 ? 'white' : 'teal.50'}
                   borderWidth="0"
                   alignItems="center"
                   lineHeight={14}
                 >
                   <Text flex={2} fontSize="xs">
-                    {myName.toUpperCase()}
+                    {abbreviateName(player.name).toUpperCase()}
                   </Text>
                   <Text flex={1} textAlign="center" fontSize="10">
                     {player.quantity_rebuy}
@@ -136,6 +126,28 @@ export default function GameScoreboard({ game, index }: Prop) {
                 </HStack>
               );
             })}
+          {showPayments && payments ?
+            <VStack>
+              <View flex={1} backgroundColor="teal.800" py={2}>
+                <Text flex={1} textAlign="center" color="white" fontWeight="semibold" fontSize="xs" onPress={() => setShowPayments(false)}>PAYMENT PLAN</Text>
+              </View>
+              <HStack py={1} backgroundColor='teal.600' alignItems="center" space={2}>
+                <Text flex={1} fontSize="xs" color="teal.100" marginLeft={2}>FROM</Text>
+                <Text flex={1} fontSize="xs" color="teal.100">TO</Text>
+                <Text flex={1} fontSize="xs" textAlign="right" color="teal.100" marginRight={2}>AMOUNT</Text>
+              </HStack>
+              {payments.map((payment: any, idx: number) => {
+                return (
+                  <HStack key={idx} py={1} backgroundColor={idx % 2 === 0 ? 'white' : 'teal.50'}>
+                    <Text flex={1} fontSize="xs" color="teal.800" marginLeft={2}>{abbreviateName(payment.from)}</Text>
+                    <Text flex={1} fontSize="xs" color="teal.800">{abbreviateName(payment.to)}</Text>
+                    <Text flex={1} fontSize="xs" textAlign="right" color="teal.800" marginRight={2}>${payment.transfer}</Text>
+                  </HStack>
+                )
+              })}
+            </VStack>
+            : null
+          }
         </VStack>
       ) : null}
     </VStack>
